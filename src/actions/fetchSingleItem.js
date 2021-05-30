8f1ebe50c13a9f681b345b4cdeee5b857c5b6689
Tresor <tresor.moise2001@gmail.com>
2021-05-30 22:49:12 +0200

    create fetchSingle action

---
+++ src/actions/fetchSingleItem.js
@@ -0,0 +1,31 @@
+import {
+  fetchSinglePending, fetchSingleItem, fetchItemsError, BASE_URL,
+} from './index';
+
+function fetchSingle(id, method="GET") {
+  console.log("SINGLE")
+  return dispatch => {
+    dispatch(fetchSinglePending());
+    const requestOptions = {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    fetch(`${BASE_URL}/items/${id}`, requestOptions)
+      .then(res => res.json())
+      .then(res => {
+        if (res.error) {
+          throw res.error;
+        }
+
+        
+        dispatch(fetchSingleItem(res));
+      })
+      .catch(error => {
+        dispatch(fetchItemsError(error));
+      });
+  };
+}
+export default fetchSingle;
