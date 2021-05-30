c6a95552eda4f431fffc6137c7caaed46f3ec534
Tresor <tresor.moise2001@gmail.com>
2021-05-30 18:43:46 +0200

    update the ItemForm container with a custom hook

--- src/containers/ItemForm.js
+++ src/containers/ItemForm.js
@@ -1,126 +1,207 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import createItem from '../actions/createItem';
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import createItem from "../actions/createItem";
+import {useInput,resetInputs} from "../helpers/index";
+import { useHistory } from "react-router-dom";
 
-class ItemForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      price: '',
-      contact: '',
-      description: '',
-      image: '',
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.onDrop = this.onDrop.bind(this);
+const ItemForm = (props) => {
+  const category=useInput('');
+  const name=useInput('');
+  const image=useInput('');
+  const price=useInput('');
+  let history = useHistory();
+  const callback=()=>{
+    resetInputs(name,category);
+    history.push('/')
   }
-
-  onDrop(picture) {
-    this.setState({ image: picture[0] });
-  }
-
-  handleChange(el) {
-    const newSate = el.target.value;
-    const prevState = this.state;
-    this.setState({ ...prevState, [el.target.name]: newSate });
-  }
-
-  handleSubmit(ev) {
-    ev.preventDefault();
-    const { store, createItem, history } = this.props;
-    const callBack = () => {
-      this.setState({
-        name: '', contact: '', price: '', description: '', image: '',
-      });
-      history.push('/items');
-    };
-    createItem(this.state, store.user.auth_token, callBack);
-  }
-
-  render() {
-    const {
-      name, contact, description, price,
-    } = this.state;
-    return (
+  const submitForm = (event) => {
+    const {createItem} = props
+    const data={
+      name: name.value,
+      category: category.value,
+      price: price.value,
+      image: image.value
+    }
+    console.log(data)
+    createItem(data,callback);
+    event.preventDefault();
+};
+  return (
+    <div className="container">
       <div>
-        <div >
-          <div >
-            <h4 >CREATE ITEM</h4>
-            <form  onSubmit={this.handleSubmit}>
-
-              <div >
-                <label >Name</label>
-                <p >
-                  <input
-
-                    type="text"
-                    required
-                    placeholder="Name"
-                    name="name"
-                    value={name}
-                    onChange={this.handleChange}
-                  />
-                  <span >
-                    <i  />
-                  </span>
-                </p>
-              </div>
-
-              <div >
-                <label >Category</label>
-                <p >
-                  <input
-
-                    type="text"
-                    required
-                    value={contact}
-                    placeholder="contact"
-                    name="contact"
-                    onChange={this.handleChange}
-                  />
-                  <span >
-                    <i  />
-                  </span>
-                </p>
-              </div>
-
-              <div >
-                <label >Price</label>
-                <p >
-                  <input
-
-                    type="number"
-                    required
-                    placeholder="Price"
-                    name="price"
-                    value={price}
-                    onChange={this.handleChange}
-                  />
-                  <span >
-                    <i  />
-                  </span>
-                </p>
-              </div>
-
-              <div >
-                <p >
-                  <button >
-                    CREATE
-                  </button>
-                </p>
-              </div>
-            </form>
+        <h4>CREATE ITEM</h4>
+        <form onSubmit={submitForm}>
+          <div className="input-cont">
+            <label>Type</label>
+            <select
+              id="category"
+              value={category.value}
+              onChange={category.onChange}
+              name="category"
+              required
+            >
+              <option value="">Select type</option>
+              <option value="Side">Side</option>
+              <option value="Main Course">Main Course</option>
+            </select>
           </div>
-        </div>
-      </div>
-    );
-  }
-}
 
-const mapStateToProps = store => ({ store });
+          <div className="input-cont">
+            <label>Name</label>
+            <input
+              type="text"
+              required
+              name="name"
+              value={name.value}
+              onChange={name.onChange}
+            />
+          </div>
+
+          <div className="input-cont">
+            <label>Price</label>
+            <input
+              type="number"
+              required
+              name="price"
+              value={price.value}
+              onChange={price.onChange}
+            />
+          </div>
+
+          <div className="input-cont">
+            <label>Image</label>
+            <input
+              type="file"
+              required
+              name="image"
+              className="custom-file-input"
+              onChange={image.handleFileChange}
+            />
+          </div>
+
+          <div>
+            <p>
+              <button type="submit" className="blue-button">
+                CREATE
+              </button>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+// class ItemForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: "",
+//       price: "",
+//       category: "",
+//       image: "",
+//     };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.onDrop = this.onDrop.bind(this);
+//   }
+
+//   onDrop(ev) {
+//     // this.setState({ image: ev.target.files[0] });
+//     // console.log(this.state);
+//     console.group("state");
+//     const prevState = this.state;
+//     let url= ev.target.files[0];
+//     this.setState({ ...prevState,image:url})
+//     console.log("the file");
+//     console.log(this.state);
+//   }
+
+//   handleChange(ev) {
+//     let prevState=this.state
+//     const newState = {...prevState,[ev.target.name]:ev.target.value};
+//     console.log(newState);
+//     this.setState(newState);
+//     console.log(this.state)
+//   }
+
+//   handleSubmit(ev) {
+//     ev.preventDefault();
+//     const { store, createItem, history } = this.props;
+//     const callBack = () => {
+//       this.setState({
+//         name: "",
+//         category: "",
+//         price: "",
+//         image: "",
+//       });
+//       history.push("/items");
+//     };
+//     createItem(this.state, callBack);
+//   }
+
+//   render() {
+//     const { name, category, price,image } = this.state;
+//     return (
+//       <div className="container">
+//         <div>
+//           <h4>CREATE ITEM</h4>
+//           <form onSubmit={this.handleSubmit}>
+//             <div className="input-cont">
+//               <label>Type</label>
+//               <select id="category" value={category} onChange={this.handleChange} name="category" required>
+//                 <option value="Side">Side</option>
+//                 <option value="Main Course">Main Course</option>
+//               </select>
+//             </div>
+
+//             <div className="input-cont">
+//               <label>Name</label>
+//               <input
+//                 type="text"
+//                 required
+//                 name="name"
+//                 value={name}
+//                 onChange={this.handleChange}
+//               />
+//             </div>
+
+//             <div className="input-cont">
+//               <label>Price</label>
+//               <input
+//                 type="number"
+//                 required
+//                 name="price"
+//                 value={price}
+//                 onChange={this.handleChange}
+//               />
+//             </div>
+
+//             <div className="input-cont">
+//               <label>Image</label>
+//               <input
+//                 type="file"
+//                 required
+//                 name="image"
+//                 className="custom-file-input"
+//                 onChange={this.onDrop}
+//               />
+//             </div>
+
+//             <div>
+//               <p>
+//                 <button className="blue-button" onClick={this.handleSubmit}>CREATE</button>
+//               </p>
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+const mapStateToProps = (store) => ({ store });
 
 const mapDispatchToProps = {
   createItem,
